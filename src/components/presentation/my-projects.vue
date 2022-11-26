4b5fb0786087c15592cb567704010ec47bbe907f
Julien Calixte <julienc@bam.tech>
2022-11-26 13:28:47 +0100

    add target blank to external websites

--- src/components/presentation/my-projects.vue
+++ src/components/presentation/my-projects.vue
@@ -4,14 +4,17 @@
 
     <ul>
       <li>
-        <a href="https://litenote.space/">Lite note</a>: highly inspired by <a
-          href="https://notes.andymatuschak.org/About_these_notes">Andy Matuschak notes' website</a>, I wanted to have a
+        <a href="https://litenote.space/" target="_blank" rel="noopener noreferrer">Lite note</a>: highly inspired by <a
+          href="https://notes.andymatuschak.org/About_these_notes" target="_blank" rel="noopener noreferrer">Andy
+          Matuschak notes' website</a>, I wanted to have a
         clean design where I can explore my notes I take on VS Code.
       </li>
-      <li><a href="https://www.npmjs.com/package/retrobus">retrobus</a>: an event bus lib I made for fun with a little
+      <li><a href="https://www.npmjs.com/package/retrobus" target="_blank" rel="noopener noreferrer">retrobus</a>: an
+        event bus lib I made for fun with a little
         extra retroactive feature.</li>
       <li>
-        <a href="https://www.npmjs.com/package/vue-pwa-asset-generator">Vue PWA Asset generator</a>: a cli command (now
+        <a href="https://www.npmjs.com/package/vue-pwa-asset-generator" target="_blank" rel="noopener noreferrer">Vue
+          PWA Asset generator</a>: a cli command (now
         a bit out of date) that generates the multiple favicons needed for a Progressive Web App.
       </li>
     </ul>
