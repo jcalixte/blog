ebfae78a5037c7b995b3b2a84ec0e4b8bbe4848b
Julien Calixte <julienc@bam.tech>
2022-12-03 14:42:15 +0100

    fixup! add prismjs

--- iles.config.ts
+++ iles.config.ts
@@ -3,10 +3,5 @@ import prism from "@islands/prism"
 
 export default defineConfig({
   siteUrl: "https://juliencalixte.eu",
-  modules: [
-    "@islands/headings",
-    prism({
-      showLineNumbers: true,
-    }),
-  ],
+  modules: ["@islands/headings", prism()],
 })
