be4afc722a63c3dff33b421914d6c8f84c147cfa
Julien Calixte <julienc@bam.tech>
2022-12-06 23:30:01 +0100

    change code font and smaller font size

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ export default defineApp({
       link: [
         {
           rel: "stylesheet",
-          href: "https://fonts.googleapis.com/css2?family=Gulzar&family=Meow+Script&family=Cousine&display=swap",
+          href: "https://fonts.googleapis.com/css2?family=Gulzar&family=Meow+Script&family=Oxygen+Mono&display=swap",
         },
       ],
     }
