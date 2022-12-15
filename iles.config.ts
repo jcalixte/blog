22bf026d5792110b909a14e39553d256cf05b716
Julien Calixte <julienc@bam.tech>
2022-12-15 11:51:05 +0100

    Install plugin for PWA!

--- iles.config.ts
+++ iles.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "iles"
 import prism from "@islands/prism"
+import pwa from "@islands/pwa"
 
 export default defineConfig({
   siteUrl: "https://juliencalixte.eu",
-  modules: ["@islands/headings", prism()],
+  modules: ["@islands/headings", prism(), pwa()],
 })
