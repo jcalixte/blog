4cccafbc0969f8408fe93e206b577975ddf19b1f
Julien Calixte <julienc@bam.tech>
2022-12-15 11:59:36 +0100

    configure PWA blog

--- iles.config.ts
+++ iles.config.ts
@@ -4,5 +4,35 @@ import pwa from "@islands/pwa"
 
 export default defineConfig({
   siteUrl: "https://juliencalixte.eu",
-  modules: ["@islands/headings", prism(), pwa()],
+  modules: [
+    "@islands/headings",
+    prism(),
+    pwa({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Julien Calixte",
+        short_name: "julienc",
+        description: "Julien Calixte's blog",
+        theme_color: "#fda7df",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 })
