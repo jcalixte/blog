cdd5d8121d552140886714ddc89aef92be44465f
Julien Calixte <julienc@bam.tech>
2023-05-27 23:04:22 +0200

    💄 (design system) add form input to custom design system

---
+++ src/components/design-system/form/FormInput.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{
+  id: string
+  label: string
+  modelValue: string
+}>()
+defineEmits<{
+  (event: "update:modelValue", value: string): void
+}>()
+</script>
+
+<template>
+  <div class="form-input">
+    <label :for="id">{{ label }}</label>
+    <input
+      type="text"
+      id="id"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target?.value ?? '')"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.form-input {
+  display: flex;
+  gap: 1rem;
+}
+</style>
