dab46cceccee1526b29947a50e66601f222f853f
Julien Calixte <julienc@bam.tech>
2023-05-29 10:30:19 +0200

    ✨ (doc jit) add doc jit project

--- src/components/presentation/my-projects.vue
+++ src/components/presentation/my-projects.vue
@@ -4,18 +4,50 @@
 
     <ul>
       <li>
-        <a href="https://litenote.space/" target="_blank" rel="noopener noreferrer">Lite note</a>: highly inspired by <a
-          href="https://notes.andymatuschak.org/About_these_notes" target="_blank" rel="noopener noreferrer">Andy
-          Matuschak notes' website</a>, I wanted to have a
-        clean design where I can explore my notes I take on VS Code.
+        <a
+          href="https://litenote.space/"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Lite note</a
+        >: highly inspired by
+        <a
+          href="https://notes.andymatuschak.org/About_these_notes"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Andy Matuschak notes' website</a
+        >, I wanted to have a clean design where I can explore my notes I take
+        on VS Code.
       </li>
-      <li><a href="https://www.npmjs.com/package/retrobus" target="_blank" rel="noopener noreferrer">retrobus</a>: an
-        event bus lib I made for fun with a little
-        extra retroactive feature.</li>
+
       <li>
-        <a href="https://www.npmjs.com/package/vue-pwa-asset-generator" target="_blank" rel="noopener noreferrer">Vue
-          PWA Asset generator</a>: a cli command (now
-        a bit out of date) that generates the multiple favicons needed for a Progressive Web App.
+        <a
+          href="https://www.npmjs.com/package/retrobus"
+          target="_blank"
+          rel="noopener noreferrer"
+          >retrobus</a
+        >: an event bus lib I made for fun with a little extra retroactive
+        feature.
+      </li>
+
+      <li>
+        <a
+          href="https://www.npmjs.com/package/vue-pwa-asset-generator"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Vue PWA Asset generator</a
+        >: a cli command (now a bit out of date) that generates the multiple
+        favicons needed for a Progressive Web App.
+      </li>
+
+      <li>
+        <a
+          href="https://marketplace.visualstudio.com/items?itemName=jcalixte.doc-jit"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Doc-JIT</a
+        >: <i>just in time</i> documentation. It displays the right
+        documentation when you need it. Doc-JIT is an VS Code extension
+        developers to open the most relevant documentation when coding features.
       </li>
     </ul>
   </section>
