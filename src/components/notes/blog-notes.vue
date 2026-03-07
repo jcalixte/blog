975f6dc46bd26e0466aa72b7d141bbf1657c0f88
Julien Calixte <julien.calixte@theodo.com>
2026-03-07 11:38:17 +0100

    feat: no last notes if there is nothing / todo with skeleton

--- src/components/notes/blog-notes.vue
+++ src/components/notes/blog-notes.vue
@@ -5,7 +5,7 @@ const notes = await fetchNotes()
 </script>
 
 <template>
-  <div class="blog-notes">
+  <div class="blog-notes" v-if="notes.length > 0">
     <h2>Last notes</h2>
     <ul>
       <li v-for="note in notes" :key="note.href">
